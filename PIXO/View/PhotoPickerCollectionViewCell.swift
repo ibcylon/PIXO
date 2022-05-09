cbfaee30653c572548e6acac047d0d979f75bd2e
kangho lee <ibcylon@naver.com>
2022-05-09 22:49:40 +0900

    Add PhotoPicker Cell

---
+++ PIXO/View/PhotoPickerCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoPickerCollectionViewCell.swift
+//  PIXO
+//
+//  Created by Kanghos on 2022/05/09.
+//
+
+import UIKit
+
+final class PhotoPickerCollectionViewCell: UICollectionViewCell {
+
+    var thumbnailImageView = UIImageView().then {
+        $0.layer.cornerRadius
+    }
+}
--- PIXO/View/PhotoPickerView.swift
+++ PIXO/View/PhotoPickerView.swift
@@ -6,5 +6,47 @@
 //
 
 import UIKit
+import SnapKit
+
+final class PickerCollectionView: UIView {
+
+    var collectionView: UICollectionView = {
+        var layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 5
+        layout.scrollDirection = .vertical
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+
+    // Code로 생성
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setUpUI()
+    }
+
+    // Bundle로 생성
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUpUI() {
+        addView()
+        setUpConstraints()
+    }
+
+    private func addView() {
+        addSubview(collectionView)
+    }
+
+    private func setUpConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 
 
+}
